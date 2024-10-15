426f5d8f7d003dd42d8a63c23da1fd5f2f4f647f
Léa BAR <pro.lea.bar@gmail.com>
2024-10-15 16:03:20 +0200

    [Fix] FIx static

--- src/web/App.ts
+++ src/web/App.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import path from "path";
 
 class App {
     private readonly _app : Application;
@@ -12,7 +13,7 @@ class App {
     }
 
     public load(port : number){
-        this._app.use(express.static("./public"))
+        this._app.use(express.static(path.resolve(__dirname, "public")))
         this._app.set("view engine", "ejs")
         this._app.listen(port)
     }
